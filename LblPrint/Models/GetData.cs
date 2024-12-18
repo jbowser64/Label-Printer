38ef5337d878af37fb477f456ee6279de20a54c5
Joseph Bowser <Joseph_Bowser@waters.com>
2024-12-18 15:13:24 -0600

    Added get data class and LabelAPI function

---
+++ LblPrint/Models/GetData.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Data.SqlTypes;
+using System.Data.SqlClient;
+using System.Data;
+using System.Net;
+using System.Text;
+
+namespace LblPrint.Models
+{
+    public class GetData()
+    {
+        public string? Material { get; set; }
+        public string? Material_description { get; set; }
+        public string? Bin { get; set; }
+
+
+        public LabelAPI()
+        {
+            byte[] zpl = Encoding.UTF8.GetBytes("^xa^cfa,50^fo100,100^fdHello World^fs^xz");
+
+            // adjust print density (8dpmm), label width (4 inches), label height (6 inches), and label index (0) as necessary
+            var request = (HttpWebRequest)WebRequest.Create("http://api.labelary.com/v1/printers/8dpmm/labels/4x6/0/");
+            request.Method = "POST";
+            // request.Accept = "application/pdf"; // omit this line to get PNG images back
+            request.ContentType = "application/x-www-form-urlencoded";
+            request.ContentLength = zpl.Length;
+
+            var requestStream = request.GetRequestStream();
+            requestStream.Write(zpl, 0, zpl.Length);
+            requestStream.Close();
+
+            try
+            {
+                var response = (HttpWebResponse)request.GetResponse();
+                var responseStream = response.GetResponseStream();
+                var fileStream = File.Create("label.PNG"); // change file name for PNG images
+                responseStream.CopyTo(fileStream);
+                responseStream.Close();
+                fileStream.Close();
+            }
+
+            catch (WebException e)
+            {
+                Console.WriteLine("Error: {0}", e.Status);
+            }
+        }
+    }
+}
+        
+    
+    
+
