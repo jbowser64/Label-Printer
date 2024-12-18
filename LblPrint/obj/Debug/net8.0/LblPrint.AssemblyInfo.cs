d4b624e6d80ea19cd854147abe69195f57ee0038
Joseph Bowser <Joseph_Bowser@waters.com>
2024-12-18 15:15:28 -0600

    Changed name

--- LblPrint/obj/Debug/net8.0/LblPrint.AssemblyInfo.cs
+++ LblPrint/obj/Debug/net8.0/LblPrint.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LblPrint")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f646a562852df6d9f56a3e0e2bd36bb3b885b50")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d81ffc0f1b4330912aed841691f19c7a3f370d28")]
 [assembly: System.Reflection.AssemblyProductAttribute("LblPrint")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LblPrint")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
