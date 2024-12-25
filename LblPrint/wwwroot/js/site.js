a80621418743e1c591dd9068a5a62c3f759e8c3e
Joseph Bowser <joebowser@student.purdueglobal.edu>
2024-12-25 14:54:32 -0600

    Messing around with JS

--- LblPrint/wwwroot/js/site.js
+++ LblPrint/wwwroot/js/site.js
@@ -2,3 +2,11 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+const partNumber = document.getElementById('partNum');
+const labelNum = document.getElementById('lblPartNum');
+function myfun() {
+    labelNum = partNumber.ariaValueText;
+}
+partNumber.addEventListener(onchange, myfun());
