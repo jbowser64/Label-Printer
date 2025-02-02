1173e02007f0d4689127824d69d29b75e1a2ca89
Joseph Bowser <joebowser@student.purdueglobal.edu>
2025-02-02 11:17:00 -0600

    Integrated Labelary API to produce png label on html page. Finally got it to work!

--- LblPrint/Controllers/HomeController.cs
+++ LblPrint/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using Microsoft.Identity.Client;
 using System.Data.Entity;
 using System.Data.Entity.Core.Common.CommandTrees.ExpressionBuilder;
 using System.Diagnostics;
+using System.Linq;
 
 namespace DataFirstTest.Controllers
 {
@@ -52,11 +53,14 @@ namespace DataFirstTest.Controllers
                         select p;
             if (!String.IsNullOrEmpty(printNum))
             {
+               
                 parts = parts.Where(s => s.Material.Contains(printNum));
-
+                //
                 foreach (var item in parts)
                 {
-                    string URL = $"http://api.labelary.com/v1/printers/8dpmm/labels/2x1/0/%5EXA%5EPW406%5EFT40,52%5EA0N,42,42%5EFH/%5EFD{item.Material}%5EFS%5EFT40,78%5EA0N,25,25%5EFH/%5EFD{item.Description}%5EFS%5EFT40,106%5EA0N,25,25%5EFH/%5EFD32%5EFS%5EFT40,140%5EA0N,37,37%5EFH/%5EFD{item.Bin}%5EFS%5EFT275,140%5EA0N,37,37%5EFH/%5EFD%5EFS%5EFT40,180%5EA0N,37,37%5EFH/%5EFD{DateTime.Now.ToString("MMMM yyyy")}%202025%5EFS%5EPQ1,0,1,Y%5EXZ";
+                   
+                    string URL = $"http://api.labelary.com/v1/printers/8dpmm/labels/2x1/0/%5EXA%5EPW406%5EFT40,52%5EA0N,42,42%5EFH/%5EFD{item.Material}%5EFS%5EFT40,78%5EA0N,25,25%5EFH/%5EFD{item.Description}%5EFS%5EFT40,106%5EA0N,25,25%5EFH/%5EFD%5EFS%5EFT40,140%5EA0N,37,37%5EFH/%5EFD{item.Bin}%5EFS%5EFT275,140%5EA0N,37,37%5EFH/%5EFD%5EFS%5EFT40,180%5EA0N,37,37%5EFH/%5EFD{DateTime.Now.ToString("MMMM yyyy")}%20%5EFS%5EPQ1,0,1,Y%5EXZ";
+                    ViewBag.url = URL; 
                 }
 
 
@@ -69,31 +73,31 @@ namespace DataFirstTest.Controllers
 
 
         }
-        public async Task GetLabelFromAPI(string printNum)
-        {
-            var parts = from p
-                       in _context.PartsAndLocations
-                        select p;
-            parts = parts.Where(s => s.Material.Contains(printNum));
+        //public async Task GetLabelFromAPI(string printNum)
+        //{
+        //    var parts = from p
+        //               in _context.PartsAndLocations
+        //                select p;
+        //    parts = parts.Where(s => s.Material.Contains(printNum));
 
-            string apiURL = ""; 
-            foreach (var item in parts)
-            { 
+        //    string apiURL = ""; 
+        //    foreach (var item in parts)
+        //    { 
                 
-            string s = $"http://api.labelary.com/v1/printers/8dpmm/labels/2x1/0/%5EXA%5EPW406%5EFT40,52%5EA0N,42,42%5EFH/%5EFD{item.Material}%5EFS%5EFT40,78%5EA0N,25,25%5EFH/%5EFD{item.Description}%5EFS%5EFT40,106%5EA0N,25,25%5EFH/%5EFD32%5EFS%5EFT40,140%5EA0N,37,37%5EFH/%5EFD{item.Bin}%5EFS%5EFT275,140%5EA0N,37,37%5EFH/%5EFD%5EFS%5EFT40,180%5EA0N,37,37%5EFH/%5EFD{DateTime.Now.ToString("MMMM yyyy")}%202025%5EFS%5EPQ1,0,1,Y%5EXZ";
-                apiURL = s; 
-            }
+        //    string s = $"http://api.labelary.com/v1/printers/8dpmm/labels/2x1/0/%5EXA%5EPW406%5EFT40,52%5EA0N,42,42%5EFH/%5EFD{item.Material}%5EFS%5EFT40,78%5EA0N,25,25%5EFH/%5EFD{item.Description}%5EFS%5EFT40,106%5EA0N,25,25%5EFH/%5EFD32%5EFS%5EFT40,140%5EA0N,37,37%5EFH/%5EFD{item.Bin}%5EFS%5EFT275,140%5EA0N,37,37%5EFH/%5EFD%5EFS%5EFT40,180%5EA0N,37,37%5EFH/%5EFD{DateTime.Now.ToString("MMMM yyyy")}%202025%5EFS%5EPQ1,0,1,Y%5EXZ";
+        //        apiURL = s; 
+        //    }
 
 
-            var response = await client.GetAsync(apiURL);
+        //    var response = await client.GetAsync(apiURL);
             
-            var responseStream = await response.Content.ReadAsStreamAsync();
+        //    var responseStream = await response.Content.ReadAsStreamAsync();
 
-            var image = File.Create("image.png");
-            responseStream.CopyTo(image);
-            responseStream.Close();
-            return (image);
-        }
+        //    var image = File.Create("image.png");
+        //    responseStream.CopyTo(image);
+        //    responseStream.Close();
+        //    return (image);
+        //}
         
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
    
