ec8123d21fb0074976292e1f0a69f08268523b6f
Joseph Bowser <Joseph_Bowser@waters.com>
2025-11-03 14:50:52 -0600

    Changed string URL to fit entire description text on labelary PNG that renders on the page.

--- LblPrint/Controllers/HomeController.cs
+++ LblPrint/Controllers/HomeController.cs
@@ -1,7 +1,9 @@
 using DataFirstTest.Models;
 using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Data.Entity.Core.Common.CommandTrees.ExpressionBuilder;
 using System.Diagnostics;
+using System.Security.Policy;
 
 namespace DataFirstTest.Controllers
 {
@@ -13,7 +15,7 @@ namespace DataFirstTest.Controllers
         {
             _context = context;
         }
-        
+
         public IActionResult PartsDB(string searchNum)
         {
             if (_context.PartsAndLocations == null)
@@ -34,7 +36,6 @@ namespace DataFirstTest.Controllers
             else
                 return View();
 
-
         }
 
         public IActionResult Index(string printNum)
@@ -49,29 +50,64 @@ namespace DataFirstTest.Controllers
                         select p;
             if (!String.IsNullOrEmpty(printNum))
             {
+                parts = parts.Where(s => s.Material.Contains(printNum) && s.Sloc.Equals("1000"));
 
-                parts = parts.Where(s => s.Material.Contains(printNum));
-                //
                 foreach (var item in parts)
                 {
+                    string description = item.Description ?? "No Description";
+                    string desc1 = description.Length > 25 ? description.Substring(0, 25) : description;
+                    string desc2 = description.Length > 25 ? description.Substring(25) : "";
 
-                    string URL = $"http://api.labelary.com/v1/printers/8dpmm/labels/2x1/0/%5EXA%5EPW406%5EFT40,52%5EA0N,42,42%5EFH/%5EFD{item.Material}" +
+                    string URL = $"https://api.labelary.com/v1/printers/8dpmm/labels/2x1/0/" +
+                       $"%5EXA%5EPW406%5EFT40,52%5EA0N,42,42%5EFH/%5EFD{item.Material}%5EFS%5EFT40,78%5EA0N,25,25%5EFH/%5E" +
+                       $"FD{desc1}%5EFS%5EFT40,106%5EA0N,25,25%5EFH/%5E" +
+                       $"FD{desc2}%5EFS%5EFT40,140%5EA0N,37,37%5EFH/%5E" +
+                       $"FD{item.Bin}%5EFS%5EFT275,140%5EA0N,37,37%5EFH/%5EFD%5EFS%5EFT40,180%5EA0N,37,37%5EFH/%5E" +
+                       $"FDFIFO: {DateTime.Now.ToString("MMMM yyyy")}%5EFS%5EPQ1,0,1,Y%5EXZ";
+
+                    /*string URL = $"http://api.labelary.com/v1/printers/8dpmm/labels/2x1/0/%5EXA%5EPW406%5EFT40,52%5EA0N,42,42%5EFH/%5EFD{item.Material}" +
                         $"%5EFS%5EFT40,78%5EA0N,25,25%5EFH/%5EFD{item.Description}" +
                         $"%5EFS%5EFT40,106%5EA0N,25,25%5EFH/%5EFD%5EFS%5EFT40,140%5EA0N,37,37%5EFH/%5EFD{item.Bin}" +
                         $"%5EFS%5EFT275,140%5EA0N,37,37%5EFH/%5EFD%5EFS%5EFT40,180%5EA0N,37,37%5EFH/%5EFD{DateTime.Now.ToString("MMMM yyyy")}%20%5EFS%5EPQ1,0,1,Y%5EXZ";
+                   */
                     ViewBag.url = URL;
                 }
-
 
                 return View(parts.ToList());
             }
 
-
             else
+
                 return View();
 
-
         }
+
+        public IActionResult PrintLabels(string printNum, int QTY, string FIFO, string Bin)
+        {
+            var parts = from p
+                       in _context.PartsAndLocations
+                       select p;
+            if (!String.IsNullOrEmpty(printNum))
+            {
+                List<string> partChanger = new List<string>();
+
+                parts = parts.Where(s => s.Material.Contains(printNum) && s.Sloc.Equals("1000"));
+            
+                foreach (var item in parts)
+                {
+                    partChanger.Add(item.ToString()); 
+                }
+                return View(parts.ToList());
+            }
+            else
+
+                return View();
+        }
+
+
+
+
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
 
