8564f4d573080c0c06274ad79260f05d2fa16ecc
Joseph Bowser <Joseph_Bowser@waters.com>
2025-11-03 15:08:28 -0600

    System updates.

--- LblPrint/obj/Debug/net8.0/LblPrint.AssemblyInfo.cs
+++ LblPrint/obj/Debug/net8.0/LblPrint.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LblPrint")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57fcdc8c0ead3e8286dd342373dc4b717d2413fc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+567ea343e050e463357e91d9ea36add370e20686")]
 [assembly: System.Reflection.AssemblyProductAttribute("LblPrint")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LblPrint")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
