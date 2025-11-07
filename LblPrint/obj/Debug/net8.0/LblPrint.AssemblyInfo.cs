4ca4de6abbd21001f4215075d2318106c78b043d
Joseph Bowser <Joseph_Bowser@waters.com>
2025-11-07 15:00:39 -0600

    Began using Cursor AI to help with formatting and code cleanup. This is the first use of Cursor and it has restructured my HTML and CSS pages.

--- LblPrint/obj/Debug/net8.0/LblPrint.AssemblyInfo.cs
+++ LblPrint/obj/Debug/net8.0/LblPrint.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LblPrint")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+567ea343e050e463357e91d9ea36add370e20686")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8564f4d573080c0c06274ad79260f05d2fa16ecc")]
 [assembly: System.Reflection.AssemblyProductAttribute("LblPrint")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LblPrint")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
